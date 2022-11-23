58f787574dadfb449143c27f51014b8990a8f58b
CarlonBaird <carlyestrella@live.com>
2022-11-23 10:26:24 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 const failingHttpRequest$ = new Observable((subscriber) => {
   setTimeout(() => {
     subscriber.error(new Error('Timeout'));
   }, 3000);
 });
+
+console.log('App started');
+
+failingHttpRequest$
+  .pipe(catchError((error) => of('Fallback value')))
+  .subscribe((value) => console.log(console.log(value)));
