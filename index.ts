812eb2b4d4f50501e72329eded28100b4e42687c
CarlonBaird <carlyestrella@live.com>
2022-11-23 10:18:39 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
+import { Observable } from 'rxjs';
 
-
+const failingHttpRequest$ = new Observable((subscriber) => {
+  setTimeout(() => {
+    subscriber.error(new Error('Timeout'));
+  }, 3000);
+});
